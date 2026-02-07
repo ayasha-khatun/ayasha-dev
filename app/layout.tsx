04eabf1aaa67cafdbbfc7f897a40f84cb5534bf8
ayasha-khatun <ayashakhatun004@gmail.com>
2026-02-07 21:10:03 +0600

    added Provider.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Provider from "@/components/HOC/Provider";
 
 const font = Inter({
   weight:['100', '200','300','400','500','600','700','800','900'],
@@ -19,11 +20,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${font.className} antialiased`}
       >
-        {children}
+        <Provider>
+          {children}
+        </Provider>
+        
       </body>
     </html>
   );
