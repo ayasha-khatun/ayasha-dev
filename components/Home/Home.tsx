cca84fdfb186f82201be00422284f8af5b6d32a8
ayasha-khatun <ayashakhatun004@gmail.com>
2026-02-08 22:54:04 +0600

    added nav links

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className='overflow-hidden'>
+    <div className="overflow-hidden">
       
     </div>
   )
--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,11 +1,26 @@
+'use client';
 import Logo from '@/components/Helper/Logo'
+import ThemeToggler from '@/components/Helper/ThemeToggler'
 import { NavLinks } from '@/Constant/Constant'
+import { Download, MenuIcon } from 'lucide-react'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Nav = () => {
+  const [navBg, setNavBg] = useState(false);
+  
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY >= 90) setNavBg(true);
+      if (window.scrollY <90) setNavBg(false);
+    };
+    window.addEventListener("scroll",handler);
+
+    return () => window.removeEventListener("scroll",handler);
+
+  }, [])
   return (
-    <div className='transition-all duration-200 h-[12vh] z-100 fixed w-full'>
+    <div className={`transition-all ${navBg ? "dark:bg-gray-800 bg-white shadow-md" : "fixed"} duration-200 h-[12vh] z-100 fixed w-full`}>
       <div className='flex items-center h-full justify-between w-[90%] xl:w[80%] mx-auto'>
         <Logo/>
         <div className=' hidden lg:flex items-center space-x-10'>
@@ -14,6 +29,17 @@ const Nav = () => {
               <p>{link.name}</p>
             </Link>
           })}
+        </div>
+        <div className='flex items-center space-x-4'>
+          <a href="#_"
+          className='box-border relative z-20 inline-flex items-center justify-center w-auto px-6 sm:px-8 py-3 overflow-hidden font-bold text-white transition-all duration-300 bg-indigo-600 rounded-md cursor-pointer group ring-offset-2 ring-1 ring-indigo-300 ring-offset-indigo-200 hover:ring-offset-indigo-500 ease focus:outline-none'>
+            <span className='relative z-20 flex items-center space-x-2 text-sm'>
+              <Download  className='w-4 h-4'/>
+            </span>
+            <span>Download Resume</span>
+          </a>
+          <ThemeToggler />
+          <MenuIcon  className='w-8 h-8 cursor-pointer text-black dark:text-white lg:hidden'/>
         </div>
       </div>
     </div>
