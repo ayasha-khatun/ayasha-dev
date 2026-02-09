202c442e857bcbed465731cc1f5f6207b5be2578
ayasha-khatun <ayashakhatun004@gmail.com>
2026-02-09 20:01:01 +0600

    added hero file

---
+++ components/Home/Hero/Hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='relative min-h-screen bg-[radial-gradient()]'>Hero</div>
+  )
+}
+
+export default Hero
--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from './Hero/Hero';
 
 function Home() {
   return (
     <div className="overflow-hidden">
-      
+      <Hero />
     </div>
   )
 }
