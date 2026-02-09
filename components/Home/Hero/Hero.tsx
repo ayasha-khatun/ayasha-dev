f179dbac4458e4b3e0ea2121e9acc8dd6e1430b3
ayasha-khatun <ayashakhatun004@gmail.com>
2026-02-10 00:16:44 +0600

    completed hero section

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,4 +1,6 @@
 "use client"
+import { Button } from '@/components/ui/button'
+import { Download, FolderOpen } from 'lucide-react'
 import React from 'react',
 import {TypeAnimation} from 'react-type-animation'
 
@@ -31,6 +33,21 @@ const Hero = () => {
             repeat={Infinity}
             className='font-mono'
             />
+          </div>
+          <p className='text-lg text-muted-foreground dark:text-gray-200 max-w-2xl mx-auto mb-10'>Crafting exceptional digital experiences with modern technologies. Passionate about building scalable applications and teaching others.</p>
+          <div className='flex flex-col sm:flex-row gap-4 justify-center'>
+            <Button size={'lg'} asChild className='w-fit mx-auto sm:mx-0'>
+              <a href="#">
+                <FolderOpen className='w-5 h-5 mr-2' />
+                View Projects
+              </a>
+            </Button>
+            <Button size={'lg'} asChild className='w-fit mx-auto sm:mx-0'>
+              <a href="#">
+                <Download className='w-5 h-5 mr-2' />
+                Download Resume
+              </a>
+            </Button>
           </div>
       </div>
     </div>
