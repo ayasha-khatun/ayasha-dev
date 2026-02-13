59f349cfe379c1c0f2eb496688a50b8f47df9ca0
ayasha-khatun <ayashakhatun004@gmail.com>
2026-02-13 19:44:35 +0600

    added projects file

--- components/Home/Project/Project.tsx
+++ components/Home/Project/Project.tsx
@@ -1,8 +1,24 @@
+import SectionHeading from '@/components/Helper/SectionHeading'
+import { projects } from '@/data'
 import React from 'react'
 
 const Project = () => {
   return (
-    <div>Project</div>
+    <div className='py-16 bg-gray-100 dark:bg-gray-200'>
+        <SectionHeading
+        title_1='Featured'
+        title_2='Projects'
+        description='A selection of my recent work and side projects'
+        />
+        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-6 w-[80%] mx-auto'>
+            {projects.map((project,index)=>{
+                return <div key={index}>
+                    
+                </div>
+            })}
+        </div>
+    </div>
+
   )
 }
 
