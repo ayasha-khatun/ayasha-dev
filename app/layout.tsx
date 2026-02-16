34b6320681a97a048fb95de04a1404dd26ab8940
ayasha-khatun <ayashakhatun004@gmail.com>
2026-02-16 23:22:05 +0600

    added scrollToTop functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Provider from "@/components/HOC/Provider";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 import Footer from "@/components/Home/Footer/Footer";
+import ScrollToTop from "@/components/Helper/ScrollToTop";
 
 const font = Inter({
   weight:['100', '200','300','400','500','600','700','800','900'],
@@ -30,6 +31,7 @@ export default function RootLayout({
           <ResponsiveNav/>
           {children}
           <Footer />
+          <ScrollToTop />
         </Provider>
         
       </body>
