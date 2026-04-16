1bfe47d24efca82575f2107a06a30a35d5432596
ayasha-khatun <ayashakhatun004@gmail.com>
2026-04-16 09:16:42 +0600

    add about page routing

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+import About from '@/components/Home/About/About';
+import React from 'react'
+
+const AboutPage = () => {
+  return <About/>;
+}
+
+export default AboutPage
---
+++ app/contact/contact.tsx
@@ -0,0 +1,8 @@
+import Contact from '@/components/Home/Contact/Contact';
+import React from 'react'
+
+const ContactPage = () => {
+  return <Contact/>;
+}
+
+export default ContactPage;
